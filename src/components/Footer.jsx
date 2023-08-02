4b039460b6a960a96bf28a8f9f12637d899d8958
MUHANNAD SALKINI <mohanad.salkini@gmail.com>
2023-08-02 09:48:44 +0300

    Edited Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,7 +12,7 @@ const Footer = () => {
     <div className="max-w-[1240px] mx-auto  text-gray-300 py-16 px-4">
       <div className="grid lg:grid-cols-3 gap-8">
         <div>
-          <h1 className="w-full text-3xl font-bold text-[#00df9a]">REACT</h1>
+          <h1 className="w-full text-3xl font-bold text-[#00df9a]">REACT.</h1>
           <p className="py-4">
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id odit
             ullam iste repellat consequatur libero reiciendis, blanditiis
@@ -65,37 +65,69 @@ const Footer = () => {
           <div>
             <h6 className="font-medium text-gray-400">Solutions</h6>
             <ul>
-              <li className="py-2 text-sm">Marketing</li>
-              <li className="py-2 text-sm">Analytics</li>
-              <li className="py-2 text-sm">Commerce</li>
-              <li className="py-2 text-sm">Insights</li>
+              <li className="py-2 text-sm hover:scale-105 hover:text-[#00df9a] duration-200 cursor-pointer">
+                <a href="/">Marketing</a>
+              </li>
+              <li className="py-2 text-sm hover:scale-105 hover:text-[#00df9a] duration-200 cursor-pointer">
+                <a href="/">Analytics</a>
+              </li>
+              <li className="py-2 text-sm hover:scale-105 hover:text-[#00df9a] duration-200 cursor-pointer">
+                <a href="/">Commerce</a>
+              </li>
+              <li className="py-2 text-sm hover:scale-105 hover:text-[#00df9a] duration-200 cursor-pointer">
+                <a href="/">Insights</a>
+              </li>
             </ul>
           </div>
           <div>
             <h6 className="font-medium text-gray-400">Support</h6>
             <ul>
-              <li className="py-2 text-sm">Pricing</li>
-              <li className="py-2 text-sm">Documentation</li>
-              <li className="py-2 text-sm">Guides</li>
-              <li className="py-2 text-sm">API Status</li>
+              <li className="py-2 text-sm hover:scale-105 hover:text-[#00df9a] duration-200 cursor-pointer">
+                <a href="/">Pricing</a>
+              </li>
+              <li className="py-2 text-sm hover:scale-105 hover:text-[#00df9a] duration-200 cursor-pointer">
+                <a href="/">Documentation</a>
+              </li>
+              <li className="py-2 text-sm hover:scale-105 hover:text-[#00df9a] duration-200 cursor-pointer">
+                <a href="/resources">Resources</a>
+              </li>
+              <li className="py-2 text-sm hover:scale-105 hover:text-[#00df9a] duration-200 cursor-pointer">
+                <a href="/">API Status</a>
+              </li>
             </ul>
           </div>
           <div>
             <h6 className="font-medium text-gray-400">Company</h6>
             <ul>
-              <li className="py-2 text-sm">About</li>
-              <li className="py-2 text-sm">Blog</li>
-              <li className="py-2 text-sm">Jobs</li>
-              <li className="py-2 text-sm">Press</li>
-              <li className="py-2 text-sm">Careers</li>
+              <li className="py-2 text-sm hover:scale-105 hover:text-[#00df9a] duration-200 cursor-pointer">
+                <a href="/about">About</a>
+              </li>
+              <li className="py-2 text-sm hover:scale-105 hover:text-[#00df9a] duration-200 cursor-pointer">
+                <a href="/blog">Blog</a>
+              </li>
+              <li className="py-2 text-sm hover:scale-105 hover:text-[#00df9a] duration-200 cursor-pointer">
+                <a href="/">Jobs</a>
+              </li>
+              <li className="py-2 text-sm hover:scale-105 hover:text-[#00df9a] duration-200 cursor-pointer">
+                <a href="/">Press</a>
+              </li>
+              <li className="py-2 text-sm hover:scale-105 hover:text-[#00df9a] duration-200 cursor-pointer">
+                <a href="/">Careers</a>
+              </li>
             </ul>
           </div>
           <div>
             <h6 className="font-medium text-gray-400">Legal</h6>
             <ul>
-              <li className="py-2 text-sm">Claim</li>
-              <li className="py-2 text-sm">Policy</li>
-              <li className="py-2 text-sm">Terms</li>
+              <li className="py-2 text-sm hover:scale-105 hover:text-[#00df9a] duration-200 cursor-pointer">
+                <a href="/company">Claim</a>
+              </li>
+              <li className="py-2 text-sm hover:scale-105 hover:text-[#00df9a] duration-200 cursor-pointer">
+                <a href="/">Policy</a>
+              </li>
+              <li className="py-2 text-sm hover:scale-105 hover:text-[#00df9a] duration-200 cursor-pointer">
+                <a href="/">Terms</a>
+              </li>
             </ul>
           </div>
         </div>
